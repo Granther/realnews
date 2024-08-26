2f7e7bed91f43407b2a1b7bf58b20dadf0eb37a7
Grant Herrema <granther321@gmail.com>
2024-08-26 15:50:48 -0400

    Leaving home

--- test.py
+++ test.py
@@ -31,14 +31,17 @@ from openai import OpenAI
 class InferenceClient():
     def __init__(
         self,
-        api_key: str,
-        model: str,
+        model: str=None,
+        api_key: str=None,
         inference_url: str=None,
         max_new_tokens: int=3000,
         global_sys_prompt: str=None,
         backend: str="groq", 
         temperature: float=1.0
     ):
+        if not model or not api_key:
+            return ValueError("Must specify model and api_key") 
+
         self.backends = ['groq', 'openai']
         self.api_key = api_key
         self.model = model
@@ -54,7 +57,7 @@ class InferenceClient():
         if backend == "groq":
             self.client = Groq(api_key=api_key)
         elif backend == "openai":
-            self.client = OpenAI(api_key=api_key)
+            self.client = OpenAI(api_key=api_key, base_url=inference_url)
 
 
     def client_infer(self, prompt, sys_prompt:str=None, model:str=None, stream:bool=False, max_new_tokens:int=None, temperature:float=None):
@@ -65,7 +68,7 @@ class InferenceClient():
 
         print(stream)
 
-        return self.client.chat.completions.create(
+        response = self.client.chat.completions.create(
             messages=[
                 {
                     "role": "system",
@@ -80,21 +83,19 @@ class InferenceClient():
             temperature=temperature,
             max_tokens=max_new_tokens,
             top_p=1,
-            stop=None,
+            # stop=None,
             stream=stream,
         )
+        print("done")
 
-        # if stream is True:
-        #     partial_message = ""
-        #     for chunk in response:
-        #         if chunk.choices[0].delta.content is not None:
-        #             partial_message = partial_message + chunk.choices[0].delta.content
-        #             yield partial_message
+        if stream is True:
+            # Returns generator func
+            return self.process_stream(response)
 
-        # elif stream is False:
-        #     print("not steras m")
-        #     return response.choices[0].message.content
+        elif stream is False:
+            return response.choices[0].message.content
 
+    # Function that is a generator
     def process_stream(self, stream):
         partial_message = ""
         for chunk in stream:
@@ -103,11 +104,11 @@ class InferenceClient():
                 yield partial_message
         
 if __name__ == "__main__":
-    inf = InferenceClient(api_key="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",
-                          model="gemma2-9b-it")
-    x = inf.client_infer("How")
-    y = inf.process_stream(x)
-    for n in y:
-        print(n)
+    # inf = InferenceClient(api_key="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",
+    #                       model="gemma2-9b-it")
+    import os
+    inf = InferenceClient(backend="openai", api_key="rc_58c93098d1b3c0ca152c4e31f6a8f7a331c7edd4959c46a6e15898faa733a87c", inference_url="https://api.featherless.ai/v1", model="NeverSleep/Llama-3-Lumimaid-8B-v0.1")
+    x = inf.client_infer("How", stream=False)
+    print(x)
 
 
